4703992c3adec310de32c7a88f3e7b32ecce5d79
Christian Honein <61818445+chonein@users.noreply.github.com>
2023-10-28 10:46:04 -0700

    added red button and battery

--- pyglet_demo/serialInput.py
+++ pyglet_demo/serialInput.py
@@ -1,5 +1,6 @@
 import serial
 from sys import platform
+import struct
 
 BAUD_RATE = 115_200
 
@@ -11,13 +12,28 @@ if platform == "darwin":
 
 
 class Input:
-    def __init__(self, on_trigger):
+    def __init__(self, on_green_trigger=None, on_red_trigger=None, on_battery_change=None):
         self.ser = serial.Serial(DEV_FILE, BAUD_RATE, timeout=0)
-        self.on_trigger = on_trigger
+        self.on_green_trigger = self._check_and_call(on_green_trigger)
+        self.on_red_trigger = self._check_and_call(on_red_trigger)
+        self.on_battery_change = self._check_and_call(on_battery_change)
+
+    def _check_and_call(self, callback):
+        if callback is not None:
+            return callback
+        return lambda: None
 
     def update(self):
         data = self.ser.read(self.ser.in_waiting)
         if len(data) > 0:
             print(data, data[-1], len(data))
-        if len(data) > 0 and data[-1] == 0x10:
-            self.on_trigger()
+            last_byte = data[-1]
+        if len(data) == 1:
+            if last_byte == 0x10:
+                self.on_green_trigger()
+            elif last_byte == 0x20:
+                self.on_red_trigger()
+        elif len(data) == 5:
+            if data[0] == 0x30:
+                [battery_level] = struct.unpack('f', data[1:])
+                self.on_battery_change(battery_level)
