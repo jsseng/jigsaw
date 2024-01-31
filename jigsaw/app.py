5015fe40f825cb0a074ea5840d37d5d00680d401
Junseo Lee <junseoleee@gmail.com>
2024-01-30 17:33:50 -0800

    initial commit for main and app

---
+++ jigsaw/app.py
@@ -0,0 +1,11 @@
+import pyglet
+import math
+import os
+import pathlib
+
+class App:
+
+    def __init__(self, x_window_size: int, y_window_size: int):
+        self.x_window_size = x_window_size
+        self.y_window_size = y_window_size
+
---
+++ jigsaw/main.py
@@ -0,0 +1,9 @@
+import app
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
