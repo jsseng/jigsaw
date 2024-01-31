94d76a448045c3042fa314acc2fb38c836cac15a
Junseo Lee <junseoleee@gmail.com>
2024-01-30 18:20:56 -0800

    window pog

--- jigsaw/main.py
+++ jigsaw/main.py
@@ -1,7 +1,11 @@
 import app
+import pyglet
 
 
 def main():
+    window = pyglet.window.Window(caption="jigsaw", fullscreen=False)
+    window = app.App(1000, 700)
+    pyglet.app.run()
     return
 
 
