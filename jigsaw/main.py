a287596a65f22117d555598c83e6311357826e7c
Junseo Lee <junseoleee@gmail.com>
2024-02-11 15:18:50 -0800

    changed to click once, click again

--- jigsaw/main.py
+++ jigsaw/main.py
@@ -1,13 +1,32 @@
 import app
 import pyglet
+from typing import Tuple
+from window import Window
 
+
+# 1024 x 768
 
 def main():
-    window = pyglet.window.Window(caption="jigsaw", fullscreen=False)
-    window = app.App(1000, 700)
+    window = Window(fullscreen=True, caption='Draggable Rectangle')
+    # image = pyglet.resource.image('images/seattle.png')
+    image = pyglet.image.load('images/seattle.png')
+    batch = pyglet.graphics.Batch()
+    # rectangle = pyglet.shapes.Rectangle(0, 0, 100, 200, color=(255, 255, 255), batch=batch)
+
+    @window.event
+    def on_draw():
+        window.clear()
+        image.blit(window.width//4, window.height//4)
+        batch.draw()
+        window.rectangle.draw()
+
     pyglet.app.run()
+
     return
 
 
 if __name__ == "__main__":
     main()
+
+
+
