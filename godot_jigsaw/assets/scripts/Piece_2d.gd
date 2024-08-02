ed73794df7abcc17082188dbb068bc7ffcfa2013
stephenlin801 <145500043+stephenlin801@users.noreply.github.com>
2024-08-02 12:11:34 -0700

    This is a cleaned up version of the previous commit
    This is a working version of the jigsaw puzzle that snaps pieces to each other rather than to a grid. I have cleaned it up for better readability and have removed files that were not ever going to be in use.

--- godot_jigsaw/assets/scripts/Piece_2d.gd
+++ godot_jigsaw/assets/scripts/Piece_2d.gd
@@ -63,12 +63,9 @@ func _process(delta):
 		set_appropriate_node()
 			
 	if selected:
-		#global_position = get_global_mouse_position()
 		#print(velocity)
 		var distance = get_global_mouse_position() - global_position
-		
-		#global_position = global_position + distance
-		#need to iterate through all the nodes and get group_number to move the appropriate ones
+		#iterate through all the nodes and get group_number to move the appropriate ones
 		#if they have the same group number as the one that has been selected then all the nodes in the same group move
 		
 		for nodes in group:
@@ -84,20 +81,6 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 			selected = true
 	
 	if PuzzleVar.active_piece: #this fixes the multiple click issue
-		
-		#try to get the velocity of he piece itself
-		#need to calculate the speed of the sprite
-		
-		#don't use mouse velocity, use sprite velocity
-		
-		#var velo = Input.get_last_mouse_velocity()
-		#print(velo)
-		#var abs_velo = Vector2(abs(velo.x),abs(velo.y))
-		
-		
-		#if abs_velo > Vector2(500,500):
-			#PuzzleVar.active_piece = 0
-			#selected = false
 			
 		if Input.is_action_just_released("click"):
 			print(Input.get_last_mouse_velocity())
@@ -120,27 +103,6 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 				print("don't throw")
 				PuzzleVar.active_piece = 0
 				selected = false
-			
-			
-		
-		#if Input.is_action_just_released("click"):
-			#if Input.get_last_mouse_velocity() < Vector2(10,10):
-				##this is not getting the position that it is truly settling in
-				#PuzzleVar.active_piece = 0
-				#selected = false
-				#var num = group_number
-				#var group = get_tree().get_nodes_in_group("puzzle_pieces")
-				##print(group_number)
-				#for nodes in group:
-					##print("getting group")
-					#if nodes.group_number == num:
-						##print("check group")
-						#nodes.check_connections(group)
-			#else:
-				#PuzzleVar.active_piece = 0
-				#selected = false
-					
-			#align_groups(group)
 
 
 func snap_and_connect(group: Array, direction: String) -> bool:
@@ -257,98 +219,3 @@ func set_appropriate_node():
 	if WID:
 		WNode = group[WID-1]
 		
-#prev is the node that was previously called, curr is the node that is currently in play
-func align_groups(group: Array): #this will trigger upon moving and releasing a piece or block and will only affect those pieces and blocks in that group
-	#start by aligning all the pieces around the selected piece so that they are all in proper line
-	group.sort_custom(sort_by_ID) #start by sorting the group
-	for nodes in group:
-		if nodes.group_number == group_number:
-			if nodes.NNode and nodes.NNode.group_number == group_number:
-				print("align north"+str(nodes.ID)+" "+str(nodes.group_number))
-				var dist = calc_distance(nodes.NCoord, nodes.NNode.SCoord)
-				if dist > .25:
-					print("Nshift")
-					dist = calc_components(nodes.NCoord, nodes.NNode.SCoord)
-					nodes.NNode.set_global_position(nodes.NNode.get_global_position() - dist)
-			if nodes.SNode and nodes.SNode.group_number == group_number:
-				print("align south"+str(nodes.ID)+" "+str(nodes.group_number))
-				var dist = calc_distance(nodes.SCoord, nodes.SNode.NCoord)
-				if dist > .25:
-					print("SShift")
-					dist = calc_components(nodes.SCoord, nodes.SNode.NCoord)
-					nodes.SNode.set_global_position(nodes.SNode.get_global_position() - dist)
-			#if nodes.ENode and nodes.ENode.group_number == group_number:
-				#print("align east"+str(nodes.ID)+" "+str(nodes.group_number))
-				#var dist = calc_distance(nodes.ECoord, nodes.ENode.WCoord)
-				#if dist > .25:
-					#print("EShift")
-					#dist = calc_components(nodes.ECoord, nodes.ENode.WCoord)
-					#nodes.ENode.set_global_position(nodes.ENode.get_global_position() - dist)
-			if nodes.WNode and nodes.WNode.group_number == group_number:
-				print("align west"+str(nodes.ID)+" "+str(nodes.group_number))
-				var dist = calc_distance(nodes.WCoord, nodes.WNode.ECoord)
-				if dist > 1:
-					print("WShift")
-					dist = calc_components(nodes.WCoord, nodes.WNode.ECoord)
-					nodes.WNode.set_global_position(nodes.WNode.get_global_position() - dist)
-	
-	
-	
-	#if curr.NNode and curr.NNode != prev:
-		#if curr.NNode.group_number == group_number:
-			#var dist = calc_components(curr.NCoord, curr.NNode.SCoord)
-			#if dist != Vector2(0,0):
-				#curr.NNode.set_global_position(curr.NNode.get_global_position() - dist)
-			#align_groups(curr, curr.NNode, group)
-	#if curr.SNode and curr.SNode != prev:
-		#if curr.SNode.group_number == group_number:
-			#var dist = calc_components(curr.SCoord, curr.SNode.NCoord)
-			#if dist != Vector2(0,0):
-				#curr.SNode.set_global_position(curr.SNode.get_global_position() - dist)
-			#align_groups(curr, curr.SNode, group)
-	#if curr.ENode and curr.ENode != prev:
-		#if curr.ENode.group_number == group_number:
-			#var dist = calc_components(curr.ECoord, curr.ENode.WCoord)
-			#if dist != Vector2(0,0):
-				#curr.ENode.set_global_position(curr.ENode.get_global_position() - dist)
-			#align_groups(curr, curr.ENode, group)
-	#if curr.WNode and curr.WNode != prev:
-		#if curr.WNode.group_number == group_number:
-			#var dist = calc_components(curr.WCoord, curr.WNode.ECoord)
-			#if dist != Vector2(0,0):
-				#curr.WNode.set_global_position(curr.WNode.get_global_position() - dist)
-			#align_groups(curr, curr.WNode, group)
-	
-	#the current issue for the above code is that when the puzzle is completed it starts to do infinite recursion
-	
-	#for nodes in group:
-		#if nodes.group_number == group_number and nodes != self: #this doesn't work currently, need to make it so that all the pieces move to align with the piece that I pick
-			#if nodes.NNode:
-				#if nodes.NNode.group_number == group_number:
-					#var dist = calc_distance(nodes.NCoord, nodes.NNode.SCoord)
-					#if dist != 0:
-						#dist = calc_components(nodes.NCoord, nodes.NNode.SCoord)
-						#nodes.NNode.set_global_position(nodes.NNode.get_global_position() - dist)
-			#if nodes.SNode:
-				#if nodes.SNode.group_number == group_number:
-					#var dist = calc_distance(nodes.SCoord, nodes.SNode.NCoord)
-					#if dist != 0:
-						#dist = calc_components(nodes.SCoord, nodes.SNode.NCoord)
-						#nodes.SNode.set_global_position(nodes.SNode.get_global_position() - dist)
-			#if nodes.ENode:
-				#if nodes.ENode.group_number == group_number:
-					#var dist = calc_distance(nodes.ECoord, nodes.ENode.WCoord)
-					#if dist != 0:
-						#dist = calc_components(nodes.ECoord, nodes.ENode.WCoord)
-						#nodes.ENode.set_global_position(nodes.ENode.get_global_position() - dist)
-			#if nodes.WNode:
-				#if nodes.WNode.group_number == group_number:
-					#var dist = calc_distance(nodes.WCoord, nodes.WNode.ECoord)
-					#if dist != 0:
-						#dist = calc_components(nodes.WCoord, nodes.WNode.ECoord)
-						#nodes.WNode.set_global_position(nodes.WNode.get_global_position() - dist)
-
-func sort_by_ID(a,b):
-	if a.get_piece_id() < b.get_piece_id():
-		return true
-	return false
