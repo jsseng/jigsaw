e6cd5963d813c6ace5a9ab2adfa19173a877740b
vhuang20 <vhuang20@calpoly.edu>
2024-08-06 23:36:52 -0700

    firebase

--- godot_jigsaw/assets/scripts/menu.gd
+++ godot_jigsaw/assets/scripts/menu.gd
@@ -13,7 +13,7 @@ func _process(delta):
 	pass
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://assets/scenes/world.tscn")
+	get_tree().change_scene_to_file("res://assets/scenes/new_menu.tcsn")
 
 func SetRow(row : int) -> void:
 	PuzzleVar.row = row
