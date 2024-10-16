042f0572c4f743b73d4eea1d816e5c2d23fdd907
Aiden Tung <aiden.tung@gmail.com>
2024-10-16 08:19:27 -0700

    add active puzzles to users and puzzles

--- godot_jigsaw/assets/scripts/FirebaseAuth.gd
+++ godot_jigsaw/assets/scripts/FirebaseAuth.gd
@@ -6,6 +6,9 @@ signal login_failed
 
 var user_id = ""
 
+# Firebase Data Model Below
+# https://lucid.app/lucidchart/af25e9e6-c77e-4969-81fa-34510e32dcd6/edit?viewport_loc=-1197%2C-1440%2C3604%2C2292%2C0_0&invitationId=inv_20e62aec-9604-4bed-b2af-4882babbe404
+
 # called when the node enters the scene tree for the first time
 func _ready() -> void:
 	Firebase.Auth.signup_succeeded.connect(_on_signup_succeeded)
@@ -23,8 +26,14 @@ func check_auth_file() -> void:
 func needs_login() -> bool:
 	return Firebase.Auth.needs_login()
 
+# get current user id
 func get_user_id() -> String:
 	return Firebase.Auth.get_user_id()
+	
+# get current user puzzle list
+func get_user_puzzle_list(user_id: String) -> FirestoreDocument:
+	var collection: FirestoreCollection = Firebase.Firestore.collection("users")
+	return (await collection.get_doc(user_id))
 	
 # handle successful anonymous login
 func _on_signup_succeeded(auth_info: Dictionary) -> void:
@@ -34,11 +43,16 @@ func _on_signup_succeeded(auth_info: Dictionary) -> void:
 	
 	# add user to firebase
 	var collection: FirestoreCollection = Firebase.Firestore.collection("users")
-	var document = await collection.add(user_id, {'active': 'true'})
+	var document = await collection.add(user_id, {'activePuzzles': ["temp"], 'lastLogin': "", "totalPlayingTime": 0})
 	print("Anonymous login succeeded. User ID: ", user_id)
+	
 
 # handle login failure
 func _on_login_failed(code: String, message: String) -> void:
 	login_failed.emit()
 	print("Login failed with code: ", code, " message: ", message)
+
+# gen id from a puzzle image to store
+func generate_id_from_string(input: String) -> int:
+	return hash(input)
 	
