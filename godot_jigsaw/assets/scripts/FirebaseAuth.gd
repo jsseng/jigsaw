80aec68a2324995592fd55816bffdbc7ff8d1f3a
Aiden Tung <115751756+tung-aiden@users.noreply.github.com>
2025-01-06 13:56:14 -0800

    fixed accidental change

--- godot_jigsaw/assets/scripts/FirebaseAuth.gd
+++ godot_jigsaw/assets/scripts/FirebaseAuth.gd
@@ -45,10 +45,10 @@ func _on_signup_succeeded(auth_info: Dictionary) -> void:
 	user_id = auth_info.get("localid") # extract the user id
 	Firebase.Auth.save_auth(auth_info)  # save auth information locally
 	logged_in.emit()
-	
+	var favorite_puzzles = [{"puzzleId": "temp", "rank": 1, "timesPlayed": 0}]
 	# add user to firebase
 	var collection: FirestoreCollection = Firebase.Firestore.collection("users")
-	var document = await collection.add(user_id, {'activePuzzles': [{"puzzleId": "0", "timeStarted": "0"}], 'lastLogin': Time.get_datetime_string_from_system(), "totalPlayingTime": 0, 'favoritePuzzles': ["temp"], 'completedPuzzles': ["temp"]})
+	var document = await collection.add(user_id, {'activePuzzles': [{"puzzleId": "0", "timeStarted": "0"}], 'lastLogin': Time.get_datetime_string_from_system(), "totalPlayingTime": 0, 'favoritePuzzles': favorite_puzzles, 'completedPuzzles': ["temp"]})
 	print("Anonymous login succeeded. User ID: ", user_id)
 		
 # write the current time to the db
